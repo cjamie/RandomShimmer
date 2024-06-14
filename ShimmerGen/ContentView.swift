debaeddb8cb25b97e1ce44c9972b4aa50895be57
Jamie Chu <huanjamierose@gmail.com>
2024-06-14 17:05:16 -0400

    Initial Commit

---
+++ ShimmerGen/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ShimmerGen
+//
+//  Created by Jamie Chu on 6/14/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ShimmerGen/ShimmerGenApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShimmerGenApp.swift
+//  ShimmerGen
+//
+//  Created by Jamie Chu on 6/14/24.
+//
+
+import SwiftUI
+
+@main
+struct ShimmerGenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
